8e54e9c74b129c82b0684930002f6804f172c3a9
shriyavanvari <vanvari.shriya@gmail.com>
2021-04-05 01:02:43 -0700

    Login Page

---
+++ Frontend/app/screens/WelcomeScreen.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpeg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/karaoke.png")} />
+        <Text style={styles.title}>DIY Karaoke</Text>
+      </View>
+      <View style={styles.loginButton}>
+        <Text style={styles.buttonText}>LOGIN</Text>
+      </View>
+      <View style={styles.signUpButton}>
+        <Text style={styles.buttonText}>SIGN UP</Text>
+      </View>
+    </ImageBackground>
+  );
+}
+//rnss shortcut to create stylesheet
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 120,
+    height: 120,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 200,
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#ff33cc",
+    bottom: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signUpButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4d4dff",
+    bottom: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "purple",
+    backgroundColor: "white",
+    textShadowColor: "gray",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 5,
+    top: 5,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
+export default WelcomeScreen;
