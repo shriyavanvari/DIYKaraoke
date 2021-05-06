ecb897453c47ec8774286ecb6d624c42be1e2cfe
Parshwa27 <parshwa.gandhi27@gmail.com>
2021-05-06 14:35:23 -0700

    Testing for windows/linux song recognition

--- DiyKaraoke/songs/views.py
+++ DiyKaraoke/songs/views.py
@@ -8,6 +8,8 @@ from rest_framework.parsers import MultiPartParser,FormParser
 from pydub import AudioSegment
 import subprocess
 from rest_framework.response import Response
+from django.http import JsonResponse
+from django.core import serializers
 
 class SongsViewSet(viewsets.ModelViewSet):
     serializer_class = SongsSerializer
@@ -16,13 +18,17 @@ class SongsViewSet(viewsets.ModelViewSet):
 
 class FileUploadView(views.APIView):
     parser_classes = [MultiPartParser,FormParser]
+    queryset = Songs.objects.all()
 
+    def post(self, request, format=None):
+        print(request.FILES['file'])
+        file_obj = request.FILES['file']
+        file_obj = AudioSegment.from_file(file_obj,format='m4a')
+        file_obj.export("audio1.mp3",format='mp3')
+        #TODO write subprocess for passing the data to songs recognition and getting the title
+        subprocess.call(['python','fingerprint/recognize-from-file.py','-f','audio1.mp3'])
+        song_obj = Songs.objects.filter(title = '7 rings')
+        song_json = serializers.serialize('json',song_obj)
+        print(type(song_json))
 
-    def post(self, request, filename, format=None):
-        print(request)
-        # file_obj = AudioSegment.from_file(file_obj,format='m4a')
-        # file_obj.export("audio1.mp3",format='mp3')
-        #
-        # process = subprocess.run(["python",''])
-
-        return Response(status=204)
+        return Response(song_json, status=204)
